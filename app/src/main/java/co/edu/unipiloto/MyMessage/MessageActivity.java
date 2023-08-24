b7c1bfb634a8fe0d4fdf7170fee877a47b12ff11
Kevin Emiro Ospinal <keospinal@hotmail.com>
2023-08-24 02:03:07 -0500

    Version 1

---
+++ app/src/main/java/co/edu/unipiloto/MyMessage/MessageActivity.java
@@ -0,0 +1,32 @@
+package co.edu.unipiloto.MyMessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent=getIntent();
+        String temp= intent.getStringExtra("mensaje");
+        TextView mess = (TextView)findViewById(R.id.Mensaje);
+        mess.setText(temp);
+    }
+
+    public void EnviarMensaje(View view) {
+        EditText Messageview = (EditText)findViewById(R.id.message);
+        String temp = Messageview.getText().toString();
+        Intent intent=new Intent(this, MessageActivity2.class);
+        intent.putExtra("mensaje",temp);
+        startActivity(intent);
+
+    }
+}
